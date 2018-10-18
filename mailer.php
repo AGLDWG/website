ff325bbe6ff789039408cc84af8786657e9b0aa0
Nicholas Car <nicholas.car@csiro.au>
2018-10-18 15:37:57 +1000

    moved to mailing with MailJet

---
+++ mailer.php
@@ -0,0 +1,62 @@
+<?php
+
+if(defined('STDIN')) {
+    include("php-mailjet-v3-simple.class.php");
+
+    $secrets = json_decode(file_get_contents("secrets.json"), true);
+    sendEmail($secrets['apiKey'], $secrets['secretKey'], 'nicholas.car@csiro.au', 'nicholas.car@csiro.au', 'fake subject2', 'this is a body');
+
+    print("Email sent\n");
+} else {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        // don't even admit this resource exists
+        http_response_code(404);
+        die();
+    } else {
+        // a POST is being sent here
+        $their_email = $_POST['their_email'];
+        $which_form = $_POST['which_form'];
+
+        include("php-mailjet-v3-simple.class.php");
+
+        if (file_exists("secrets.json")) {
+            $secrets = json_decode(file_get_contents("secrets.json"), true);
+            if (sendEmail($secrets['apiKey'], $secrets['secretKey'], 'nicholas.car@csiro.au', $their_email, '[agldwg] $which_form', 'this is a body')) {
+                http_response_code(201);
+                print("Email sent");
+            } else {
+                http_response_code(500);
+                print("Email not sent");
+                die();
+            }
+        } else {
+            http_response_code(500);
+            print("secrets.json file missing\n");
+            die();
+        }
+    }
+}
+
+
+// from https://github.com/mailjet/mailjet-apiv3-php-simple#a-function-to-send-an-email
+function sendEmail($apiKey, $secretKey, $from, $to, $subject, $text)
+{
+    $mj = new Mailjet($apiKey, $secretKey);
+    $params = array(
+        "method" => "POST",
+        "from" => $from,
+        "to" => $to,
+        "subject" => $subject,
+        "text" => $text
+    );
+
+    $result = $mj->sendEmail($params);
+
+    if ($mj->_response_code == 200)
+       return true;
+    else
+       return false;
+}
+
+
+?>
